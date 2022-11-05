fbc5515c1498adbf81c95557f6acc63d5f05e1b4
Rohit-Bishnoi29 <rohit_12013099@nitkkr.ac.in>
2022-11-05 16:33:56 +0530

    First Commit

--- server/app.js
+++ server/app.js
@@ -1,22 +1,28 @@
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
-app.get("/",(req,res)=>{
-res.send("Hello Wolrd from the server");
+
+const middleware = (req,res,next)=>{
+    console.log("Hello from Middleware ");
+    next();
+}
+
+app.get("/", (req, res) => {
+    res.send("Hello Wolrd from the server");
 });
-app.get("/about",(req,res)=>{
+app.get("/about",middleware, (req, res) => {
     res.send("Hello About Wolrd from the server");
-    });
-    app.get("/contact",(req,res)=>{
-        res.send("Hello Contact Wolrd from the server");
-        });
-        app.get("/signin",(req,res)=>{
-            res.send("Hello Signin Wolrd from the server");
-            });
-            app.get("/signup",(req,res)=>{
-                res.send("Hello Signup Wolrd from the server");
-                });
+});
+app.get("/contact", (req, res) => {
+    res.send("Hello Contact Wolrd from the server");
+});
+app.get("/signin", (req, res) => {
+    res.send("Hello Signin Wolrd from the server");
+});
+app.get("/signup", (req, res) => {
+    res.send("Hello Signup Wolrd from the server");
+});
 const PORT = 3000;
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
